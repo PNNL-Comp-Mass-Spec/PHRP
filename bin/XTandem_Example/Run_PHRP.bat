f2746c0192613070e36ae4110a4569ffd7d0f5ba
Matthew Monroe <matthew.monroe@pnnl.gov>
2014-04-10 04:23:07 +0000

    Add example X!Tandem data

---
+++ bin/XTandem_Example/Run_PHRP.bat
@@ -0,0 +1,3 @@
+..\PeptideHitResultsProcRunner.exe /i:QC_Shew_12_02_pt5_2c_20Dec12_Leopard_12-11-10_xt.xml /m:xtandem_Rnd1PartTryp_Rnd2DynMetOx_ModDefs.txt /n:xtandem_Rnd1PartTryp_Rnd2DynMetOx.xml /t:Mass_Correction_Tags.txt /L /ProteinMods /F:Shewanella_oneidensis_MR1_2010-04-22_Tryp_Pig_Bov.fasta
+
+pause
