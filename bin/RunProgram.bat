59c3f9045bfe4b8227f6294a9a8ef0205390f98c
alchemistmatt <matthew.monroe@pnnl.gov>
2014-07-17 12:02:45 -0700

    Update post-build command

--- bin/RunProgram.bat
+++ bin/RunProgram.bat
@@ -9,7 +9,10 @@ rem PeptideHitResultsProcRunner.exe /i:INS200901081452_Auto358179\QC_Shew_07_02_
 
 rem PeptideHitResultsProcRunner.exe /i:INS200902131714_Auto365855\Bats_0012g_OrbiA_19Dec07_Draco_07-09-13_inspect.txt /m:INS200902131714_Auto365855\Inspect_MetOx_FTHybrid_50ppm_ModDefs.txt /n:INS200902131714_Auto365855\Inspect_MetOx_FTHybrid_50ppm.txt /t:INS200902131714_Auto365855\Mass_Correction_Tags.txt /L
 
-PeptideHitResultsProcRunner.exe /i:MSGFPlus_Example\QC_Shew_13_05b_HCD_500ng_24Mar14_Tiger_14-03-04_msgfplus.tsv /m:MSGFPlus_Example\MSGFDB_PartTryp_MetOx_20ppmParTol_ModDefs.txt /n:MSGFPlus_Example\MSGFDB_PartTryp_MetOx_20ppmParTol.txt /t:MSGFPlus_Example\Mass_Correction_Tags.txt /L /ProteinMods /F:MSGFPlus_Example\Shewanella_oneidensis_MR1_2010-04-22_Tryp_Pig_Bov_decoy.fasta
+PeptideHitResultsProcRunner.exe /i:MSGFPlus_Example\QC_Shew_13_05b_HCD_500ng_24Mar14_Tiger_14-03-04_msgfplus.tsv /m:MSGFPlus_Example\MSGFDB_PartTryp_MetOx_20ppmParTol_ModDefs.txt /n:MSGFPlus_Example\MSGFDB_PartTryp_MetOx_20ppmParTol.txt /t:MSGFPlus_Example\Mass_Correction_Tags.txt /L /ProteinMods /F:MSGFPlus_Example\Shewanella_oneidensis_MR1_2010-04-22_Tryp_Pig_Bov.revcat.fasta
+
+rem cd x64
+rem PeptideHitResultsProcRunner.exe /i:..\MSGFPlus_Example\QC_Shew_13_05b_HCD_500ng_24Mar14_Tiger_14-03-04_msgfplus.tsv /m:..\MSGFPlus_Example\MSGFDB_PartTryp_MetOx_20ppmParTol_ModDefs.txt /n:..\MSGFPlus_Example\MSGFDB_PartTryp_MetOx_20ppmParTol.txt /t:..\MSGFPlus_Example\Mass_Correction_Tags.txt /L /ProteinMods /F:..\MSGFPlus_Example\Shewanella_oneidensis_MR1_2010-04-22_Tryp_Pig_Bov.revcat.fasta
 
 
 pause
