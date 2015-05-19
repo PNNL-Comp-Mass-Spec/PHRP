3aed79356be120c6eaf8c2fcdd62122bb53b157e
alchemistmatt <matthew.monroe@pnnl.gov>
2015-05-18 20:37:10 -0700

    Remove x64 solutions since now compiling as AnyCPU

--- bin/Distribute_Files.bat
+++ bin/Distribute_Files.bat
@@ -1,15 +1,16 @@
 copy PeptideHitResultsProcessor.dll "F:\My Documents\Projects\DataMining\DMS_Managers\Analysis_Manager\AM_Common"
 copy PeptideHitResultsProcessor.dll "F:\My Documents\Projects\DataMining\DMS_Managers\Analysis_Manager\AM_Program\bin"
 
-copy x64\*.dll "F:\My Documents\Projects\DataMining\DMS_Managers\Analysis_Manager\AM_Common\PHRP"
-copy x64\PeptideHitResultsProcessor.pdb "F:\My Documents\Projects\DataMining\DMS_Managers\Analysis_Manager\AM_Common\PHRP"
-copy x64\PeptideHitResultsProcRunner.exe "F:\My Documents\Projects\DataMining\DMS_Managers\Analysis_Manager\AM_Common\PHRP"
-copy x64\PeptideHitResultsProcRunner.pdb "F:\My Documents\Projects\DataMining\DMS_Managers\Analysis_Manager\AM_Common\PHRP"
+rem PeptideHitResultsProcRunner is compiled as AnyCPU
+copy *.dll "F:\My Documents\Projects\DataMining\DMS_Managers\Analysis_Manager\AM_Common\PHRP"
+copy PeptideHitResultsProcessor.pdb "F:\My Documents\Projects\DataMining\DMS_Managers\Analysis_Manager\AM_Common\PHRP"
+copy PeptideHitResultsProcRunner.exe "F:\My Documents\Projects\DataMining\DMS_Managers\Analysis_Manager\AM_Common\PHRP"
+copy PeptideHitResultsProcRunner.pdb "F:\My Documents\Projects\DataMining\DMS_Managers\Analysis_Manager\AM_Common\PHRP"
 
-copy x64\*.dll "C:\DMS_Programs\PHRP"
-copy x64\PeptideHitResultsProcessor.pdb "C:\DMS_Programs\PHRP"
-copy x64\PeptideHitResultsProcRunner.exe "C:\DMS_Programs\PHRP"
-copy x64\PeptideHitResultsProcRunner.pdb "C:\DMS_Programs\PHRP"
+copy *.dll "C:\DMS_Programs\PHRP"
+copy PeptideHitResultsProcessor.pdb "C:\DMS_Programs\PHRP"
+copy PeptideHitResultsProcRunner.exe "C:\DMS_Programs\PHRP"
+copy PeptideHitResultsProcRunner.pdb "C:\DMS_Programs\PHRP"
 
 
 
