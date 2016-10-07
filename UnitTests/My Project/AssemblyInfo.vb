c49f1d249701bedabb10e657d4c824c26370d50f
alchemistmatt <matthew.monroe@pnnl.gov>
2016-10-06 20:19:26 -0700

    Add unit test project

---
+++ UnitTests/My Project/AssemblyInfo.vb
@@ -0,0 +1,30 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("PHRP_UnitTests")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("PNNL")>
+<Assembly: AssemblyProduct("PHRP_UnitTests")>
+<Assembly: AssemblyCopyright("Copyright © PNNL 2016")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("21a2f566-73f6-4471-9d43-94b1d65100be")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+<Assembly: AssemblyVersion("1.0.*")> 
