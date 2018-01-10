0b8918b2f1624744fb57d892bd8cfa8c66519238
alchemistmatt <matthew.monroe@pnnl.gov>
2018-01-10 11:01:56 -0800

    Update batch files for processing example data

--- Data/MSGFPlus_Example/Run_PHRP.bat
+++ Data/MSGFPlus_Example/Run_PHRP.bat
@@ -14,7 +14,7 @@ echo.
 echo Procesing with %ExePath%
 echo.
 
-%ExePath% /i:QC_Shew_12_02_pt5_2c_20Dec12_Leopard_12-11-10_xt.xml /m:xtandem_Rnd1PartTryp_Rnd2DynMetOx_ModDefs.txt /n:xtandem_Rnd1PartTryp_Rnd2DynMetOx.xml /t:Mass_Correction_Tags.txt /L /ProteinMods /F:Shewanella_oneidensis_MR1_2010-04-22_Tryp_Pig_Bov.fasta
+%ExePath% /i:QC_Shew_13_05b_HCD_500ng_24Mar14_Tiger_14-03-04_msgfplus.tsv /m:MSGFDB_PartTryp_MetOx_20ppmParTol_ModDefs.txt /n:MSGFDB_PartTryp_MetOx_20ppmParTol.txt /t:Mass_Correction_Tags.txt /L /ProteinMods /F:Shewanella_oneidensis_MR1_2010-04-22_Tryp_Pig_Bov.fasta
 
 :Done
 
