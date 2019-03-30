def8220be0d6fc8c1df0bdbbb091f8e65659b96e
alchemistmatt <matthew.monroe@pnnl.gov>
2019-03-29 17:52:06 -0700

    Add batch files

---
+++ Data/TestPHRP_Inspect.bat
@@ -0,0 +1,3 @@
+xcopy \\gigasax\DMS_FASTA_File_Archive\dynamic\forward\ID_002614_23305E80.fasta FASTA\ /D
+..\bin\Debug\PeptideHitResultsProcRunner.exe TestData\INS201108111004_Auto731708\QC_Shew_11_03_0pt5_a_10Aug11_Cougar_11-01-17_inspect.txt /O:TestData\INS201108111004_Auto731708_Compare\ /M:TestData\INS201108111004_Auto731708\Inspect_MetOx_FTHybrid_20ppm_ModDefs.txt /N:TestData\INS201108111004_Auto731708\Inspect_MetOx_FTHybrid_20ppm.txt /SynPvalue:0.2  /SynProb:0.05  /L:PHRP_LogFile.txt /ProteinMods /F:FASTA\ID_002614_23305E80.fasta
+
