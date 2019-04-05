4ece4b2be861218816144d3d2c1a8fa0212600bd
alchemistmatt <matthew.monroe@pnnl.gov>
2019-04-04 18:39:35 -0700

    Update relative paths

--- Data/TestPHRP_TopPIC.bat
+++ Data/TestPHRP_TopPIC.bat
@@ -1,2 +1,2 @@
-xcopy \\gigasax\DMS_FASTA_File_Archive\dynamic\forward\ID_005649_5ECA3D02.fasta FASTA\ /D
-..\bin\Debug\PeptideHitResultsProcRunner.exe TestData\TPC201808171745_Auto1624200\MZ20170525_Mnx_PFA_TopPIC_PrSMs.txt  /O:TestData\TPC201808171745_Auto1624200_Compare\ /N:TestData\TPC201808171745_Auto1624200\TopPIC_15ppmParTol_NumShift1_2018-08-16.txt /SynPvalue:0.2  /SynProb:0.05  /L:PHRP_LogFile.txt /ProteinMods /F:FASTA\ID_005649_5ECA3D02.fasta
+xcopy \\gigasax\DMS_FASTA_File_Archive\dynamic\forward\ID_005649_5ECA3D02.fasta TestData\FASTA\ /D
+..\bin\Debug\PeptideHitResultsProcRunner.exe TestData\TPC201808171745_Auto1624200\MZ20170525_Mnx_PFA_TopPIC_PrSMs.txt  /O:TestData\TPC201808171745_Auto1624200_Compare\ /N:TestData\TPC201808171745_Auto1624200\TopPIC_15ppmParTol_NumShift1_2018-08-16.txt /SynPvalue:0.2  /SynProb:0.05  /L:PHRP_LogFile.txt /ProteinMods /F:TestData\FASTA\ID_005649_5ECA3D02.fasta
