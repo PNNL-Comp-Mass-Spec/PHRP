92f4a7e245298404482789ee1ac3fe18483445d3
alchemistmatt <matthew.monroe@pnnl.gov>
2019-07-10 17:59:38 -0700

    Update batch files

--- Data/XTandem_Example/Run_PHRP.bat
+++ Data/XTandem_Example/Run_PHRP.bat
@@ -14,7 +14,7 @@ echo.
 echo Procesing with %ExePath%
 echo.
 
-%ExePath% /i:QC_Shew_12_02_pt5_2c_20Dec12_Leopard_12-11-10_xt.xml /m:xtandem_Rnd1PartTryp_Rnd2DynMetOx_ModDefs.txt /n:xtandem_Rnd1PartTryp_Rnd2DynMetOx.xml /t:Mass_Correction_Tags.txt /L /ProteinMods /F:Shewanella_oneidensis_MR1_2010-04-22_Tryp_Pig_Bov.fasta
+%ExePath% -i:QC_Shew_12_02_pt5_2c_20Dec12_Leopard_12-11-10_xt.xml -m:xtandem_Rnd1PartTryp_Rnd2DynMetOx_ModDefs.txt -n:xtandem_Rnd1PartTryp_Rnd2DynMetOx.xml -t:Mass_Correction_Tags.txt -L -ProteinMods -F:Shewanella_oneidensis_MR1_2010-04-22_Tryp_Pig_Bov.fasta > PHRP_ConsoleOutput.txt
 
 :Done
 
