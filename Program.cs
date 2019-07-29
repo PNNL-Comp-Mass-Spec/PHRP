bbd597214277a15a31fb0604476a3828091024a3
alchemistmatt <matthew.monroe@pnnl.gov>
2019-07-29 15:02:41 -0700

    Update program date

--- Program.cs
+++ Program.cs
@@ -26,7 +26,7 @@ namespace PeptideHitResultsProcRunner
 {
     static class Program
     {
-        public const string PROGRAM_DATE = "July 23, 2019";
+        public const string PROGRAM_DATE = "July 26, 2019";
 
         private static string mInputFilePath;
         private static string mOutputDirectoryPath;                      // Optional
