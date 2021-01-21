dd1f11c05c63c830c42b9ae00408e0478a402e15
alchemistmatt <matthew.monroe@pnnl.gov>
2021-01-20 16:25:28 -0800

    Suppress warnings

---
+++ PHRPReader/GlobalSuppressions.cs
@@ -0,0 +1,13 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Design", "RCS1075:Avoid empty catch clause that catches System.Exception.", Justification = "<Pending>", Scope = "member", Target = "~M:PHRPReader.clsPeptideCleavageStateCalculator.InitializeRegExObjects")]
+[assembly: SuppressMessage("Design", "RCS1075:Avoid empty catch clause that catches System.Exception.", Justification = "<Pending>", Scope = "member", Target = "~M:PHRPReader.clsPeptideMassCalculator.ComputeSequenceMass(System.String)~System.Double")]
+[assembly: SuppressMessage("Design", "RCS1075:Avoid empty catch clause that catches System.Exception.", Justification = "<Pending>", Scope = "member", Target = "~M:PHRPReader.clsPeptideMassCalculator.ConvertAminoAcidSequenceToEmpiricalFormula(System.String)~PHRPReader.clsEmpiricalFormula")]
+[assembly: SuppressMessage("Design", "RCS1075:Avoid empty catch clause that catches System.Exception.", Justification = "<Pending>", Scope = "member", Target = "~M:PHRPReader.clsPHRPParser.IsNumber(System.String)~System.Boolean")]
+[assembly: SuppressMessage("Design", "RCS1075:Avoid empty catch clause that catches System.Exception.", Justification = "<Pending>", Scope = "member", Target = "~M:PHRPReader.clsPHRPReader.IsNumber(System.String)~System.Boolean")]
+[assembly: SuppressMessage("Readability", "RCS1123:Add parentheses when necessary.", Justification = "<Pending>", Scope = "member", Target = "~M:PHRPReader.clsPeptideMassCalculator.ConvoluteMass(System.Double,System.Int32,System.Int32,System.Double)~System.Double")]
