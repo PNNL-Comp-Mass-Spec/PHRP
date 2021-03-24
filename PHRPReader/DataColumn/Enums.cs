17dfc4d3cfb9dd54343818abed7512224fa6f5fb
alchemistmatt <matthew.monroe@pnnl.gov>
2021-03-24 12:02:27 -0700

    Remove unused using statements

--- PHRPReader/DataColumn/Enums.cs
+++ PHRPReader/DataColumn/Enums.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace PHRPReader.DataColumn
+﻿namespace PHRPReader.DataColumn
 {
     public class Enums
     {
