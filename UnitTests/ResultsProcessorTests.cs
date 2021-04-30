5864ca5c1c76611a54a3b547aabdb333ced7490d
alchemistmatt <matthew.monroe@pnnl.gov>
2021-04-29 20:32:41 -0700

    Fix typo in test case filename

--- UnitTests/ResultsProcessorTests.cs
+++ UnitTests/ResultsProcessorTests.cs
@@ -23,7 +23,7 @@ namespace PHRP_UnitTests
         [TestCase("QC", "QC_Shew_16_01-15f_08_4Nov16_Tiger_16-02-14", "QC_PP_MCF-7_21_01_d_22Apr21_Rage_Rep-21-02-06")]
         [TestCase("QC", "QC_Shew_16_01-15f_08_4Nov16_Tiger_16-02-14", "QC_Shew_16_01-15f_09_15Nov16_Tiger_16-02-15", "QC_PP_MCF-7_21_01_d_22Apr21_Rage_Rep-21-02-06")]
         [TestCase("QC", "QC_Shew_16_01-15f_09_15Nov16_Tiger_16-02-15", "QC_Mam_19_01_a_22Apr21_Rage_Rep-21-02-06")]
-        [TestCase("QC", "QC_PP_MCF -7_21_01_d_22Apr21_Rage_Rep-21-02-06", "QC_Mam_19_01_a_22Apr21_Rage_Rep-21-02-06")]
+        [TestCase("QC", "QC_PP_MCF-7_21_01_d_22Apr21_Rage_Rep-21-02-06", "QC_Mam_19_01_a_22Apr21_Rage_Rep-21-02-06")]
         [TestCase("", "QC_Shew_16_01-15f_08_4Nov16_Tiger_16-02-14", "AALEDTLAETEAR_MSonly")]
         [TestCase("AALEDTLAETEAR_MS", "AALEDTLAETEAR_MSMSonly", "AALEDTLAETEAR_MSonly")]
         [TestCase("Acetamiprid_rep1_1000V", "Acetamiprid_rep1_1000V", "Acetamiprid_rep1_1000V_neg")]
