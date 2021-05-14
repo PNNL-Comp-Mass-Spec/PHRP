f6b85c012e255949878b15280aa1a2321da35344
alchemistmatt <matthew.monroe@pnnl.gov>
2021-05-13 21:30:15 -0700

    Update class that holds data read from a MaxQuant synopsis file

--- PeptideHitResultsProcessor/Data/MaxQuantResults.cs
+++ PeptideHitResultsProcessor/Data/MaxQuantResults.cs
@@ -1,90 +1,264 @@
 ﻿using PHRPReader;
 using PHRPReader.Data;
+using System.Collections.Generic;
 
 namespace PeptideHitResultsProcessor.Data
 {
     /// <summary>
-    /// This class is used to track the peptide details for a MaxQuant search result
-    /// See SearchResultsBaseClass for additional information
+    /// This class is used to track the peptide details for a MaxQuant search result loaded from a synopsis file
     /// </summary>
+    /// <remarks>
+    /// See SearchResultsBaseClass for additional information
+    /// </remarks>
     public class MaxQuantResults : SearchResultsBaseClass
     {
-        // Ignore Spelling: Acetyl, MaxQuant
-
-        public string DatasetName { get; set; }
-        public int DatasetID { get; set; }
-        public string MissedCleavageCount { get; set; }
+        // Ignore Spelling: Acetyl, Carbamidomethyl, Da, MaxQuant, Orbitrap, terminii, tryptic
 
         /// <summary>
-        /// Comma-separated list of modification names that are in the ModifiedSequence field
+        /// Dataset name
+        /// </summary>
+        /// <remarks>This is an abbreviated form of the full dataset name</remarks>
+        public string DatasetName { get; set; }
+
+        /// <summary>
+        /// Dataset ID
+        /// </summary>
+        public int DatasetID { get; set; }
+
+        /// <summary>
+        /// Type of fragmentation used to create the MS/MS spectrum
+        /// </summary>
+        /// <remarks>
+        /// Types:
+        ///   CID - Collision Induced Dissociation
+        ///   HCD - High energy Collision induced Dissociation
+        ///   ETD - Electron Transfer Dissociation
+        /// </remarks>
+        public string FragMethod { get; set; }
+
+        /// <summary>
+        /// Index of the spectrum in the dataset (1-based, consecutive integer)
+        /// </summary>
+        public string SpecIndex { get; set; }
+
+        /// <summary>
+        /// Precursor ion m/z (theoretical value, not observed value)
+        /// </summary>
+        /// <remarks>
+        /// <para>
+        /// This is the theoretical m/z of the first isotope of the isotopic distribution of the parent ion
+        /// </para>
+        /// <para>
+        /// For example, given a 3+ parent ion whose isotopic distribution in the MS1 spectrum
+        /// has ions at 827.769, 828.105, 828.438, and 828.772 m/z, the most intense ion is the 828.434 ion.
+        /// The instrument might then isolate that ion for fragmentation, giving a spectrum label for the MS2 spectrum of
+        /// "Full ms2 828.44@cid35.00"
+        /// </para>
+        /// <para>
+        /// MS-GF+ reports the precursor m/z as 827.76965, which is the observed m/z value
+        /// MaxQuant reports the PrecursorMZ as 827.76176, which is the theoretical value based on the identified peptide (TAHEVRPGNVIMFEGSPWVVQK)
+        /// </para>
+        /// <para>
+        /// Example 2: given a 3+ parent ion whose isotopic distribution in the MS1 spectrum
+        /// has ions at 755.402, 755.737, 756.071, and 756.407 m/z, the most intense ion is the 755.737 ion.
+        /// The instrument might then isolate that ion for fragmentation, giving a spectrum label for the MS2 spectrum of
+        /// "Full ms2 755.74@cid35.00"
+        /// </para>
+        /// <para>
+        /// MS-GF+ reports the precursor m/z as 755.40515, which is the observed m/z value
+        /// MaxQuant reports the PrecursorMZ as 755.39497, which is the theoretical value based on the identified peptide (IINIGSVVGTMGNAGQVNYSAAK)
+        /// MaxQuant reports the PrecursorMZ as 755.39497, which is the theoretical value based on the identified peptide (IINIGSVVGTMGNAGQVNYSAAK)
+        /// </para>
+        /// </remarks>
+        public string PrecursorMZ { get; set; }
+
+        /// <summary>
+        /// Mass error, in Da, as computed by MaxQuant
+        /// </summary>
+        /// <remarks>
+        /// This is NaN if a peptide has Type=MSMS
+        /// In contrast, if a peptide has Type=MULTI-MSMS, a value will be defined
+        /// </remarks>
+        public string MaxQuantComputedDelM { get; set; }
+
+        /// <summary>
+        /// Mass error of the precursor ion equivalent monoisotopic mass value
+        /// vs. the predicted monoisotopic mass of the identified peptide sequence,
+        /// as computed by MaxQuant
+        /// </summary>
+        /// <remarks>
+        /// This is NaN if a peptide has Type=MSMS
+        /// In contrast, if a peptide has Type=MULTI-MSMS, a value will be defined
+        /// </remarks>
+        public string MaxQuantComputedDelMPPM { get; set; }
+
+        /// <summary>
+        /// Monoisotopic (M+H)+ value, computed from PrecursorMZ and Charge
+        /// </summary>
+        public string MH { get; set; }
+
+        /// <summary>
+        /// Theoretical monoisotopic mass of the identified sequence (uncharged, including mods), as computed by MaxQuant
+        /// </summary>
+        public string CalculatedMonoMass { get; set; }
+
+        /// <summary>
+        /// Comma-separated list of dynamic modification names and affected residue number
         /// </summary>
         /// <remarks>
         /// Examples:
-        ///   Acetyl (Protein N-term)
-        ///   Oxidation (M)
-        ///   Acetyl (Protein N-term),Oxidation (M)
-        ///   2 Oxidation (M)
-        ///   4 Oxidation (M)
+        /// Oxidation 7
+        /// Acetyl 1,Oxidation 4
         /// </remarks>
         public string Modifications { get; set; }
 
-        public string ModifiedSequence { get; set; }
-        public string CollisionMode { get; set; }
+        /// <summary>
+        /// Protein(s) associated with this peptide
+        /// </summary>
+        public List<string> Proteins { get; }
 
         /// <summary>
-        /// Mass analyzer
+        /// Name of the best scoring protein this peptide is associated with
         /// </summary>
         /// <remarks>
-        /// FTMS, ITMS
+        /// Typically there is only one protein name here
+        /// However, in cases of a tied score, will be a semicolon separated list
         /// </remarks>
-        public string MassAnalyzer { get; set; }
+        public string LeadingRazorProtein { get; set; }
 
         /// <summary>
-        /// Identification type
+        /// Number of tryptic terminii
         /// </summary>
-        /// <remarks>
-        /// MULTI-MSMS, MULTI-SECPEP, or MSMS
-        /// </remarks>
-        public string IdType { get; set; }
-        public string ScanEvent { get; set; }
-        public string IsotopeIndex { get; set; }
-        public string Precursor_mz { get; set; }
-        public string MaxQuantComputedDelMPPM { get; set; }
-        public string MaxQuantComputedDelM { get; set; }
-        public string SingleMassErrorPPM { get; set; }
-        public string ScanTimeMinutes { get; set; }
+        public string NTT { get; set; }
 
         /// <summary>
         /// Posterior error probability
         /// </summary>
         /// <remarks>
-        /// Lower is better
+        /// Similar to p-value
+        /// Smaller values (closer to zero) are higher confidence
         /// </remarks>
         public string PEP { get; set; }
 
         /// <summary>
-        /// Confidence score; higher is better
+        /// Andromeda score for the best MS/MS spectrum with this peptide
         /// </summary>
+        /// <remarks>
+        /// Higher scores are better
+        /// </remarks>
         public string Score { get; set; }
-        public string DeltaScore { get; set; }
-        public string ScoreDiff { get; set; }
-        public string LocalizationProb { get; set; }
-        public string Combinatorics { get; set; }
-        public string PIF { get; set; }
-        public string PrecursorScan { get; set; }
-        public string PrecursorIntensity { get; set; }
-        public string MatchedFragmentIonCount { get; set; }
-        public string IntensityCoverage { get; set; }
-        public string PeakCoverage { get; set; }
-        public string IsReversedProtein { get; set; }
 
         /// <summary>
-        /// Semicolon separated list of protein group IDs
+        /// Score difference to the second best identified peptide with a different amino acid sequence
         /// </summary>
+        public string DeltaScore { get; set; }
+
+        /// <summary>
+        /// Summed up extracted ion current (XIC) of all isotopic clusters associated with this peptide
+        /// </summary>
+        /// <remarks>
+        /// From the peptides.txt file, column "Intensity"
+        /// </remarks>
+        public string PeptideIntensity { get; set; }
+
+        /// <summary>
+        /// Mass Analyzer of the instrument
+        /// </summary>
+        /// <remarks>
+        /// Types:
+        ///   ITMS - Ion trap
+        ///   FTMS - Fourier transform ICR or Orbitrap
+        ///   TOF - Time of flight
+        /// </remarks>
+        public string MassAnalyzer { get; set; }
+
+        /// <summary>
+        /// Type of precursor ion as identified by MaxQuant
+        /// </summary>
+        /// <remarks>
+        /// <para>
+        /// Prefixes:
+        ///   ISO - isotopic cluster.
+        ///   PEAK - single peak.
+        ///   MULTI - labeling cluster.
+        /// </para>
+        /// <para>
+        /// Examples:
+        /// MULTI-MSMS
+        /// MULTI-SECPEP
+        /// MSMS
+        /// </para>
+        /// </remarks>
+        public string PrecursorType { get; set; }
+
+        /// <summary>
+        /// Elution time of the MS/MS spectrum
+        /// </summary>
+        public string RetentionTime { get; set; }
+
+        /// <summary>
+        /// Scan number where the precursor ion was observed
+        /// </summary>
+        public string PrecursorScanNumber { get; set; }
+
+        /// <summary>
+        /// Intensity of the precursor ion in the scan that it was observed
+        /// </summary>
+        public string PrecursorIntensity { get; set; }
+
+        /// <summary>
+        /// Number of peaks (MS/MS ions) matching to the predicted fragmentation spectrum
+        /// </summary>
+        public string NumberOfMatches { get; set; }
+
+        /// <summary>
+        /// Fraction of intensity in the MS/MS spectrum that is annotated
+        /// </summary>
+        public string IntensityCoverage { get; set; }
+
+        /// <summary>
+        /// Number of missed enzymatic cleavages
+        /// </summary>
+        public string MissedCleavageCount { get; set; }
+
+        /// <summary>
+        /// Unique (consecutive) identifier for each row in msms.txt
+        /// </summary>
+        /// <remarks>
+        /// Used to cross-link the information in msms.txt with information stored in other files
+        /// </remarks>
+        public string MsMsID { get; set; }
+
+        /// <summary>
+        /// Identifier of the protein-group this redundant peptide sequence is associated with
+        /// </summary>
+        /// <remarks>
+        /// <para>
+        /// Typically a single number, but could be a semicolon separated list
+        /// if a peptide is associated with multiple protein groups
+        /// </para>
+        /// <para>
+        /// Can be used to look up the extended protein information in the proteinGroups.txt file
+        /// </para>
+        /// </remarks>
         public string ProteinGroupIDs { get; set; }
 
+        /// <summary>
+        /// The identifier of the non-redundant peptide sequence
+        /// </summary>
+        /// <remarks>
+        /// Corresponds to the id column in the peptides.txt file
+        /// </remarks>
         public string PeptideID { get; set; }
+
+        /// <summary>
+        /// Identifier referencing a row in the modificationSpecificPeptides.txt file
+        /// </summary>
         public string ModPeptideID { get; set; }
+
+        /// <summary>
+        /// Identifier referencing a row in the evidence.txt file
+        /// </summary>
         public string EvidenceID { get; set; }
 
         /// <summary>
@@ -99,6 +273,7 @@ namespace PeptideHitResultsProcessor.Data
         public MaxQuantResults(PeptideModificationContainer peptideMods, PeptideMassCalculator peptideSeqMassCalculator)
             : base(peptideMods, peptideSeqMassCalculator)
         {
+            Proteins = new List<string>();
         }
 
         public override void Clear()
@@ -107,30 +282,30 @@ namespace PeptideHitResultsProcessor.Data
 
             DatasetName = string.Empty;
             DatasetID = 0;
-            MissedCleavageCount = string.Empty;
-            CollisionMode = string.Empty;
-            MassAnalyzer = string.Empty;
-            IdType = string.Empty;
-            ScanEvent = string.Empty;
-            IsotopeIndex = string.Empty;
-            Precursor_mz = string.Empty;
-            MaxQuantComputedDelMPPM = string.Empty;
+            FragMethod = string.Empty;
+            SpecIndex = string.Empty;
+            PrecursorMZ = string.Empty;
             MaxQuantComputedDelM = string.Empty;
-            SingleMassErrorPPM = string.Empty;
-            ScanTimeMinutes = string.Empty;
+            MaxQuantComputedDelMPPM = string.Empty;
+            MH = string.Empty;
+            CalculatedMonoMass = string.Empty;
+            Modifications = string.Empty;
+            Proteins.Clear();
+            LeadingRazorProtein = string.Empty;
+            NTT = string.Empty;
             PEP = string.Empty;
             Score = string.Empty;
             DeltaScore = string.Empty;
-            ScoreDiff = string.Empty;
-            LocalizationProb = string.Empty;
-            Combinatorics = string.Empty;
-            PIF = string.Empty;
-            PrecursorScan = string.Empty;
+            PeptideIntensity = string.Empty;
+            MassAnalyzer = string.Empty;
+            PrecursorType = string.Empty;
+            RetentionTime = string.Empty;
+            PrecursorScanNumber = string.Empty;
             PrecursorIntensity = string.Empty;
-            MatchedFragmentIonCount = string.Empty;
+            NumberOfMatches = string.Empty;
             IntensityCoverage = string.Empty;
-            PeakCoverage = string.Empty;
-            IsReversedProtein = string.Empty;
+            MissedCleavageCount = string.Empty;
+            MsMsID = string.Empty;
             ProteinGroupIDs = string.Empty;
             PeptideID = string.Empty;
             ModPeptideID = string.Empty;
