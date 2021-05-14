b4a87d1110d81b152107993144cd182752060021
alchemistmatt <matthew.monroe@pnnl.gov>
2021-05-13 21:29:25 -0700

    Add logic to associate dynamic and static modifications with specific residues

---
+++ PeptideHitResultsProcessor/Data/MaxQuantModifiedSequenceModInfo.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace PeptideHitResultsProcessor.Data
+{
+    internal class MaxQuantModifiedSequenceModInfo
+    {
+        // Ignore Spelling: Acetyl
+
+        public string MaxQuantModName { get; }
+
+        public string MatchedModName { get; set; }
+
+        public string MatchedModNameWithParentheses => string.IsNullOrWhiteSpace(MatchedModName) ? string.Empty : string.Format("({0})", MatchedModName);
+
+        /// <summary>
+        /// For mods with names like "Oxidation (M)" or "Acetyl (Protein N-term)", return "Oxidation" or "Acetyl"
+        /// Otherwise, return the full name
+        /// </summary>
+        public string GetModNameWithoutResidues()
+        {
+            // Look for a space followed by an open parenthesis
+            var charIndex = MaxQuantModName.IndexOf(" (", StringComparison.Ordinal);
+            if (charIndex < 0)
+                return MaxQuantModName;
+
+            return MaxQuantModName.Substring(0, charIndex);
+        }
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="maxQuantModName"></param>
+        public MaxQuantModifiedSequenceModInfo(string maxQuantModName)
+        {
+            MaxQuantModName = maxQuantModName;
+            MatchedModName = string.Empty;
+        }
+    }
+}
