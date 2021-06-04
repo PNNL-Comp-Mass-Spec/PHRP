efc703149fe777d285a3df836d2631516f8c5783
alchemistmatt <matthew.monroe@pnnl.gov>
2021-06-04 15:51:52 -0700

    Add MaxQuant example

---
+++ Data/MaxQuant_Example/Run_PHRP.bat
@@ -0,0 +1,21 @@
+@echo off
+
+set ExePath=PeptideHitResultsProcRunner.exe
+
+if exist %ExePath% goto DoWork
+if exist ..\%ExePath% set ExePath=..\%ExePath% && goto DoWork
+if exist ..\..\Bin\Debug\%ExePath% set ExePath=..\..\Bin\Debug\%ExePath% && goto DoWork
+
+echo Executable not found: %ExePath%
+goto Done
+
+:DoWork
+echo.
+echo Procesing with %ExePath%
+echo.
+
+%ExePath% /i:msms.txt /n:MaxQuant_Tryp_Stat_CysAlk_Dyn_MetOx_NTermAcet_20ppmParTol.xml /t:Mass_Correction_Tags.txt /L /ProteinMods /F:M_musculus_UniProt_SPROT_2013_09_2013-09-18_Tryp_Pig_Bov.fasta > PHRP_ConsoleOutput.txt
+
+:Done
+
+pause
