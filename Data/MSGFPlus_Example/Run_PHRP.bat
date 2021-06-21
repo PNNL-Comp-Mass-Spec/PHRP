7c7aa7541d12e40dad1c322242eac43c427f2cf0
alchemistmatt <matthew.monroe@pnnl.gov>
2021-06-21 16:12:54 -0700

    Update MS-GF+ parameter file name in batch file

--- Data/MSGFPlus_Example/Run_PHRP.bat
+++ Data/MSGFPlus_Example/Run_PHRP.bat
@@ -14,7 +14,7 @@ echo.
 echo Procesing with %ExePath%
 echo.
 
-%ExePath% /i:QC_Shew_13_05b_HCD_500ng_24Mar14_Tiger_14-03-04_msgfplus.tsv /m:MSGFDB_PartTryp_MetOx_20ppmParTol_ModDefs.txt /n:MSGFDB_PartTryp_MetOx_20ppmParTol.txt /t:Mass_Correction_Tags.txt /L /ProteinMods /F:Shewanella_oneidensis_MR1_2010-04-22_Tryp_Pig_Bov.fasta > PHRP_ConsoleOutput.txt
+%ExePath% /i:QC_Shew_13_05b_HCD_500ng_24Mar14_Tiger_14-03-04_msgfplus.tsv /m:MSGFPlus_PartTryp_MetOx_20ppmParTol_ModDefs.txt /n:MSGFPlus_PartTryp_MetOx_20ppmParTol.txt /t:Mass_Correction_Tags.txt /L /ProteinMods /F:Shewanella_oneidensis_MR1_2010-04-22_Tryp_Pig_Bov.fasta > PHRP_ConsoleOutput.txt
 
 :Done
 
