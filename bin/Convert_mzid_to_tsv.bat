3dd404b741a9659a2e67d93f913b4e38875a5559
alchemistmatt <matthew.monroe@pnnl.gov>
2022-01-13 11:02:40 -0800

    Update path in batch file and remove old batch fle

--- bin/Convert_mzid_to_tsv.bat
+++ bin/Convert_mzid_to_tsv.bat
@@ -1,2 +1,9 @@
-java.exe  -Xmx1000M -cp C:\DMS_Programs\MSGFDB\MSGFPlus.jar edu.ucsd.msjava.ui.MzIDToTsv -i MSGFPlus_Job1217459\48488_Aniger_BU_F9_C_8Jun15_Tiger_15-03-05_msgfplus.mzid -o MSGFPlus_Job1217459\48488_Aniger_BU_F9_C_8Jun15_Tiger_15-03-05_msgfplus.tsv -showQValue 1 -showDecoy 1 -unroll 1
+@echo off
+echo Option 1: use the Mzid-To-Tsv-Converter standalone application, available at https://github.com/PNNL-Comp-Mass-Spec/Mzid-To-Tsv-Converter/releases
+
+echo Option 2: use MSGFPlus.jar
+
+@echo on
+java.exe -Xmx1000M -cp C:\DMS_Programs\MSGFPlus\MSGFPlus.jar edu.ucsd.msjava.ui.MzIDToTsv -i MSGFPlus_Job1217459\48488_Aniger_BU_F9_C_8Jun15_Tiger_15-03-05_msgfplus.mzid -o MSGFPlus_Job1217459\48488_Aniger_BU_F9_C_8Jun15_Tiger_15-03-05_msgfplus.tsv -showQValue 1 -showDecoy 1 -unroll 1
+
 pause
