2223deec7d6ba875fddaebfe385b1d2dbd628369
alchemistmatt <matthew.monroe@pnnl.gov>
2022-01-13 16:04:46 -0800

    Remove unused using statements

--- PeptideHitResultsProcessor/Processor/MaxQuantResultsProcessor.cs
+++ PeptideHitResultsProcessor/Processor/MaxQuantResultsProcessor.cs
@@ -15,7 +15,6 @@ using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
 using System.Linq;
-using System.Text;
 using System.Text.RegularExpressions;
 using System.Xml.Linq;
 using PeptideHitResultsProcessor.Data;
--- PeptideHitResultsProcessor/Processor/MultiDatasetResultsProcessor.cs
+++ PeptideHitResultsProcessor/Processor/MultiDatasetResultsProcessor.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
-using PeptideHitResultsProcessor.SearchToolResults;
 using PRISM;
 
 namespace PeptideHitResultsProcessor.Processor
