454f7a2f3bf789902458518361406bd744dc5d80
alchemistmatt <matthew.monroe@pnnl.gov>
2022-01-17 18:43:06 -0800

    Remove old batch files

--- PeptideHitResultsProcessor_SourceCode/SourceAndSupportingDLLs/Update_Supporting_Source_Code.bat
+++
@@ -1,7 +0,0 @@
-@echo on
-rem Del PRISM_Class_Library_Source*.zip
-
-@echo off
-echo;
-
-rem Copy "F:\My Documents\Projects\DataMining\DMS_Managers\PRISM_Class_Library\PRISM_Class_Library_Source*.zip" .
