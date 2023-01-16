1023ecc9bc81f36f08bbeaf47892ccdd1bb4da31
alchemistmatt <matthew.monroe@pnnl.gov>
2023-01-15 16:13:54 -0800

    Update packages from NuGet

--- PeptideHitResultsProcessor/Processor/PHRPBaseClass.cs
+++ PeptideHitResultsProcessor/Processor/PHRPBaseClass.cs
@@ -49,7 +49,7 @@ namespace PeptideHitResultsProcessor.Processor
         /// <summary>
         /// Program date
         /// </summary>
-        public const string PROGRAM_DATE = "September 12, 2022";
+        public const string PROGRAM_DATE = "January 15, 2023";
 
         /// <summary>
         /// Constructor
