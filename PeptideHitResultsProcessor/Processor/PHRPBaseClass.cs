866794a384c97847503218d8a1aff2160c1cfa99
alchemistmatt <matthew.monroe@pnnl.gov>
2024-07-19 20:11:50 -0700

    Update program date

--- PeptideHitResultsProcessor/Processor/PHRPBaseClass.cs
+++ PeptideHitResultsProcessor/Processor/PHRPBaseClass.cs
@@ -50,7 +50,7 @@ namespace PeptideHitResultsProcessor.Processor
         /// <summary>
         /// Program date
         /// </summary>
-        public const string PROGRAM_DATE = "January 26, 2024";
+        public const string PROGRAM_DATE = "July 19, 2024";
 
         /// <summary>
         /// Constructor
